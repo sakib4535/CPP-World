8d1e6afda30acb053c396e52dd965714f2599e38
Galactic Commander <95187806+sakib4535@users.noreply.github.com>
2023-04-19 05:20:54 +0600

    Add files via upload

---
+++ C++ Basics/Calculator/string_analysis.h
@@ -0,0 +1,15 @@
+#ifndef STRING_ANALYSIS_H
+#define STRING_ANALYSIS_H
+
+#include <string>
+
+int get_string_length(std::string input);
+int count_words(std::string input);
+int count_vowels(std::string input);
+int count_consonants(std::string input);
+int count_digits(std::string input);
+int count_special_chars(std::string input);
+int sum_of_digits(std::string input);
+double square_root_of_sum_of_digit_squares(std::string input);
+
+#endif
